c8eff9a1c40a795d109b12361fb9496e8e9c616b
Brandi Meza <bmeza2@cnm.edu>
2021-10-13 09:17:49 -0600

    foreach file

---
+++ data-types/data-types.js
@@ -0,0 +1,19 @@
+// string
+const name = 'Fred'
+const anothername = "Jim"
+const yetAnotherName = `Mary and ${name}`
+
+// number
+const age = 18
+
+const zipcode = '87109'
+
+// arrays
+const cars = ['ford', 'dodge', 'toyota']
+const empty = []
+
+console.log(cars[0])
+console.log(cars[2])
+
+// objects... JSON
+const person = {name: 'Fred', age:18}
