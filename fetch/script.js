00cf73be0d72337912e876de2029e283a07ccb51
Brandi Meza <bmeza2@cnm.edu>
2021-11-01 10:39:49 -0600

    fetch code along

---
+++ fetch/script.js
@@ -0,0 +1,17 @@
+const grabBacon = () => {
+    const targetElement = document.getElementById
+    ('target')
+
+    fetch("https://baconipsum.com/api/?type=all-meat&paras=6&start-with-lorem=1")
+        .then(response => response.json())
+        .then(data => {
+            const mappedArray = data.map(index => {
+                console.log(index)
+                return `<p> ${index} </p>`
+                })
+                targetElement.innerHTML = mappedArray.join
+                ("\n")
+            })
+}
+
+window.onload = grabBacon()
